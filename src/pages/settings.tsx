17c1f19ad2b8c3553d0ed5096863bb92451aea96
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-10-11 13:12:40 +0300

    Update settings.tsx

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import Layout from 'src/components/Layout'
 
 const Settings: NextPage = () => {
   return (
     <Layout>
+      <div className="flex flex-row mb-5 text-3xl">
+          <Link href="/">
+              <a>Orders / </a>
+          </Link>
+          <h1 className='ml-2'>Settings</h1>
+      </div>
       <div>
 
       </div>
