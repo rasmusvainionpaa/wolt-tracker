b3fc71a03838f47ee9aad4bc5f5b36577931ed16
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-10-11 07:59:10 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,31 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import Counters from 'src/components/Counter'
 import Layout from 'src/components/Layout'
+import Order from 'src/components/Order'
+import { OrderType } from 'src/types/order'
 
 const Home: NextPage = () => {
+  const [orders, setOrders] = useState<OrderType[]>([])
+
+  useEffect(() => {
+    setOrders(JSON.parse(localStorage.getItem("orders") || "[]"))
+  }, [])
+
   return (
     <Layout>
-      <div>
-
+      <h1 className="mb-5 text-3xl">Orders</h1>
+      <Counters data={orders} />
+      <div className='flex justify-center'>
+        { orders.length < 1 ? <p>Loading...</p> :
+          <ul className='flex flex-col'>
+            {orders.map((ordered, index) => (
+              <li key={index} className="">
+                <Order order={ordered}/>
+              </li>
+            ))}
+          </ul>
+        }
       </div>
     </Layout>
   )
