c5503d0610a08c5f8886b88197e4444a4f1b0b74
Rasmus Vainionpää <46357949+RasmusVainionpaa@users.noreply.github.com>
2022-10-31 12:15:19 +0200

    Update add.tsx
    added reset function to form that cleans form after submit

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -9,6 +9,7 @@ const Add: NextPage = () => {
   const {
     register,
     handleSubmit,
+    reset,
     watch,
     formState: { errors },
   } = useForm<OrderType>();
@@ -18,13 +19,14 @@ const Add: NextPage = () => {
   const onSubmit: SubmitHandler<OrderType> = async (data) => {
     try {
       await fetch(`/api/order`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
-      })
-    }  catch (error) {
-      console.error(error)
+      });
+    } catch (error) {
+      console.error(error);
     }
+    reset();
   };
 
   return (
@@ -36,72 +38,72 @@ const Add: NextPage = () => {
       </div>
 
       <div className="flex justify-center">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex justify-center max-w-3/4 w-72 flex-col bg-blue-50 p-5 rounded-lg"
-          >
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="store">Store</label>
-              <input
-                id="store"
-                className="w-full rounded-md"
-                {...register("store", { required: true })}
-              />
-              {errors.store && (
-                <span className="text-red-400">This field is required</span>
-              )}
-            </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="max-w-3/4 flex w-72 flex-col justify-center rounded-lg bg-blue-50 p-5"
+        >
+          <div className="mb-4 flex flex-col">
+            <label htmlFor="store">Store</label>
+            <input
+              id="store"
+              className="w-full rounded-md"
+              {...register("store", { required: true })}
+            />
+            {errors.store && (
+              <span className="text-red-400">This field is required</span>
+            )}
+          </div>
 
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="price">Price</label>
-              <input
-                id="price"
-                className="w-full rounded-md"
-                type="number"
-                step="0.01"
-                {...register("price", { required: true })}
-              />
-              {errors.price && (
-                <span className="text-red-400">This field is required</span>
-              )}
-            </div>
+          <div className="mb-4 flex flex-col">
+            <label htmlFor="price">Price</label>
+            <input
+              id="price"
+              className="w-full rounded-md"
+              type="number"
+              step="0.01"
+              {...register("price", { required: true })}
+            />
+            {errors.price && (
+              <span className="text-red-400">This field is required</span>
+            )}
+          </div>
 
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="ordertype">Order type</label>
-              <select
-                id="ordertype"
-                className="rounded-md"
-                {...register("order_type", { required: true })}
-              >
-                <option value="store">Store</option>
-                <option value="restaurant">Restaurant</option>
-              </select>
-
-              {errors.order_type && (
-                <span className="text-red-400">This field is required</span>
-              )}
-            </div>
-
-            <div className="mb-4 flex flex-col">
-              <label htmlFor="ordered_at">Date ordered</label>
-              <input
-                id="ordered_at"
-                className="w-full rounded-md"
-                type="date"
-                {...register("ordered_at", { required: true })}
-              />
-              {errors.ordered_at && (
-                <span className="text-red-400">This field is required</span>
-              )}
-            </div>
-
-            <button
-              type="submit"
-              className={"flex rounded bg-blue-500 px-2 py-1 text-white w-12"}
+          <div className="mb-4 flex flex-col">
+            <label htmlFor="ordertype">Order type</label>
+            <select
+              id="ordertype"
+              className="rounded-md"
+              {...register("order_type", { required: true })}
             >
-              Save
-            </button>
-          </form>
+              <option value="store">Store</option>
+              <option value="restaurant">Restaurant</option>
+            </select>
+
+            {errors.order_type && (
+              <span className="text-red-400">This field is required</span>
+            )}
+          </div>
+
+          <div className="mb-4 flex flex-col">
+            <label htmlFor="ordered_at">Date ordered</label>
+            <input
+              id="ordered_at"
+              className="w-full rounded-md"
+              type="date"
+              {...register("ordered_at", { required: true })}
+            />
+            {errors.ordered_at && (
+              <span className="text-red-400">This field is required</span>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            className={"flex w-12 rounded bg-blue-500 px-2 py-1 text-white"}
+          >
+            Save
+          </button>
+        </form>
       </div>
     </Layout>
   );
